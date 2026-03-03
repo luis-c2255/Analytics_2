45d540360c549c4ee0ff82319a50e5d92083092a
luis-c2255 <lmodesto63@gmail.com>
2026-03-03 23:11:05 +0100

    add

--- pages/3_🆙_Yahoo_Stock_Analysis_Dashboard.py
+++ pages/3_🆙_Yahoo_Stock_Analysis_Dashboard.py
@@ -438,7 +438,7 @@ st.markdown("   ")
 st.subheader("💹 :green[Volume vs Price Movement]", divider="green")
 fig_scatter = go.Figure()
 colors_scatter = ["green" if x > 0 else 'red' for x in filtered_df['Daily_Return']]
-fig_scattter.add_trace(go.Scatter(
+fig_scatter.add_trace(go.Scatter(
     x=filtered_df['Volume'],
     y=filtered_df['Daily_Return'].abs(),
     mode='markers',
