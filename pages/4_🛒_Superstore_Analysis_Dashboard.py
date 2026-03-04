07d6055e4ee116fb1257a10d12c90e4d32c7406a
luis-c2255 <lmodesto63@gmail.com>
2026-03-05 00:01:32 +0100

    add

--- pages/4_🛒_Superstore_Analysis_Dashboard.py
+++ pages/4_🛒_Superstore_Analysis_Dashboard.py
@@ -4,6 +4,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import numpy as np
+import openpyxl
 
 from utils.theme import Components, Colors, apply_chart_theme, init_page
 
