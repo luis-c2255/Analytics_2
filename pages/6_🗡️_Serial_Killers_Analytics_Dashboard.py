de3d4e70b77c29f6f19c0743b93ab1cd29602ebe
luis-c2255 <lmodesto63@gmail.com>
2026-03-11 02:19:19 +0100

    add

--- pages/6_🗡️_Serial_Killers_Analytics_Dashboard.py
+++ pages/6_🗡️_Serial_Killers_Analytics_Dashboard.py
@@ -352,7 +352,7 @@ fig7 = px.pie(
     values=victim_category_counts.values,
     names=victim_category_counts.index,
     title='Distribution by Victim Count Category',
-    color_discrete_sequence=px.colors.sequential.Reds_r,
+    color_discrete_sequence=px.colors.sequential.oranges,
     hole=0.4
 )
 fig7.update_traces(textposition='inside', textinfo='percent+label')
